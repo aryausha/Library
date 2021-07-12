33bbffe07a7316e053f93d3b6e730732327a8196
Aryamol A U <mearyamol@gmail.com>
2021-07-12 13:35:22 +0530

    add background colour

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,26 +9,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
-<body>
+<body class="pic">
     <table class="table table-borderless">
         <tr>
-            <td>BOOKNAME</td>
+            <td style="color:rgb(255, 238, 0);">BOOKNAME</td>
             <td><input type="text" class="form-control"></td>
         </tr>
         <tr>
-            <td>AUTHORNAME</td>
+            <td style="color:rgb(255, 238, 0);">AUTHORNAME</td>
             <td><input type="text" class="form-control"></td>
         </tr>
         <tr>
-            <td>DISTRIBUTOR</td>
+            <td style="color:rgb(255, 238, 0);">DISTRIBUTOR</td>
             <td><input type="text" class="form-control"></td>
         </tr>
         <tr>
-            <td>PUBLISHER</td>
+            <td style="color:rgb(255, 238, 0);">PUBLISHER</td>
             <td><input type="text" class="form-control"></td>
         </tr>
         <tr>
-            <td>MRP</td>
+            <td style="color:rgb(255, 238, 0);">MRP</td>
             <td><input type="text" class="form-control"></td>
         </tr>
         
